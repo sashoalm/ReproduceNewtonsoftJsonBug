3a012b17e1746f3c883467380e93f4aaf0360de2
Alexander Almaleh <aalmaleh@objectsystems.com>
2017-06-21 14:52:58 +0300

    Reproduce bug.

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ClassLibrary1;
 
 using Newtonsoft.Json;
 
-namespace ConsoleApp5
+namespace ClassLibrary1
 {
-    class Program
+    public class Class1
     {
-        static void Main(string[] args)
+        public static void Foo()
         {
             var json = JsonConvert.SerializeObject("foo");
             Console.WriteLine(json);
-
-            // Now call the DLL.
-            Class1.Foo();
         }
     }
 }
